63b7ae755472da9c3cf94a5c7f5e7bb63565580a
Naresh Madiraju <naresh.johnny@gmail.com>
2020-01-05 22:20:33 -0500

    Removed old public and private cidrs

--- variables.tf
+++ variables.tf
@@ -4,20 +4,15 @@ variable "region" { default = "ap-south-1"}
 variable "create_vpc" { default     = true}
 variable "name" { default     = "myvpc"}
 variable "cidr" { default     = "10.10.0.0/16"}
+variable "enable_ipv6" { default     = false}
+variable "enable_dns_support" { default     = true}
+variable "enable_dns_hostnames" { default     = true}
+variable "instance_tenancy" { default     = "default"}
 
 variable "public_azs" { default = ["ap-south-1a"]}
 variable "public_subnets" { default = ["10.10.10.0/24"]}
 variable "private_azs" { default = ["ap-south-1a", "ap-south-1b"]}
 variable "private_subnets" { default = ["10.10.60.0/24", "10.10.70.0/24"]}
-
-
-
-variable "enable_ipv6" { default     = false}
-variable "enable_dns_support" { default     = true}
-variable "enable_dns_hostnames" { default     = true}
-variable "instance_tenancy" { default     = "default"}
-variable "public_subnet_cidr" { default = "10.10.0.0/24" }
-variable "private_subnet_cidr" { default = "10.10.1.0/24" }
 
 
 variable "common_tags" {
