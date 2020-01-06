a01d22ff166834bcde1171329dfb60ff2a6b7a57
Naresh Madiraju <naresh.johnny@gmail.com>
2020-01-05 20:11:50 -0500

    VPC

---
+++ main.tf
@@ -0,0 +1,100 @@
+provider "aws"{
+
+  region = var.region
+
+}
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "this" {
+
+  cidr_block                       = var.cidr
+  instance_tenancy                 = var.instance_tenancy
+  enable_dns_hostnames             = var.enable_dns_hostnames
+  enable_dns_support               = var.enable_dns_support
+  assign_generated_ipv6_cidr_block = var.enable_ipv6
+
+  tags = "${merge(
+    var.common_tags,
+    var.vpc_tags,
+    map(
+      "Name", "${var.name}"
+    )
+  )}"
+}
+
+resource "aws_internet_gateway" "this" {
+    vpc_id = "${aws_vpc.this.id}"
+    tags = "${merge(
+      var.common_tags,
+      var.igw_tags,
+      map(
+        "Name", "${var.name}-igw"
+      )
+    )}"
+}
+
+resource "aws_subnet" "public" {
+
+    count = "${length(var.public_subnets)}"
+    vpc_id = "${aws_vpc.this.id}"
+    availability_zone = "${var.public_azs[count.index]}"
+    cidr_block        = "${var.public_subnets[count.index]}"
+    map_public_ip_on_launch = true
+
+    tags = "${merge(
+      var.common_tags,
+      var.vpc_tags,
+      map(
+        "Name", "${var.name}-public-${count.index}"
+      )
+    )}"
+}
+
+resource "aws_subnet" "private" {
+
+    count = "${length(var.private_subnets)}"
+    vpc_id = "${aws_vpc.this.id}"
+    availability_zone = "${var.private_azs[count.index]}"
+    cidr_block        = "${var.private_subnets[count.index]}"
+    map_public_ip_on_launch = false
+
+    tags = "${merge(
+      var.common_tags,
+      var.vpc_tags,
+      map(
+        "Name", "${var.name}-private-${count.index}"
+      )
+    )}"
+}
+
+
+
+
+#Public Route Table
+resource "aws_route_table" "public" {
+
+  count = length(var.public_subnets) > 0 ? 1 : 0
+  vpc_id = "${aws_vpc.this.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.this.id}"
+  }
+
+  tags = "${merge(
+    var.common_tags,
+    var.public_route_table_tags,
+    map(
+      "Name", "${var.name}-public-${count.index}"
+    )
+  )}"
+
+}
+
+# Route table associations
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets) > 0 ? length(var.public_subnets) : 0
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = element(aws_route_table.public.*.id,0)
+}
---
+++ variables.tf
@@ -0,0 +1,63 @@
+
+variable "region" { default = "ap-south-1"}
+
+variable "create_vpc" { default     = true}
+variable "name" { default     = "myvpc"}
+variable "cidr" { default     = "10.10.0.0/16"}
+
+variable "public_azs" { default = ["ap-south-1a"]}
+variable "public_subnets" { default = ["10.10.10.0/24"]}
+variable "private_azs" { default = ["ap-south-1a", "ap-south-1b"]}
+variable "private_subnets" { default = ["10.10.60.0/24", "10.10.70.0/24"]}
+
+
+
+variable "enable_ipv6" { default     = false}
+variable "enable_dns_support" { default     = true}
+variable "enable_dns_hostnames" { default     = true}
+variable "instance_tenancy" { default     = "default"}
+variable "public_subnet_cidr" { default = "10.10.0.0/24" }
+variable "private_subnet_cidr" { default = "10.10.1.0/24" }
+
+
+variable "common_tags" {
+  type        = map(string)
+  default = {
+      system_created = "terraform",
+      auto_delete = "true"
+      business_unit = "corp",
+      department =  "cloud-engineering",
+      team = "cloud-engineering",
+      cost_center ="1002345",
+    }
+}
+
+variable "vpc_tags" {
+  type        = map(string)
+  default     = {}
+}
+
+variable "igw_tags" {
+  type        = map(string)
+  default     = {}
+}
+
+variable "public_subnet_tags" {
+  type        = map(string)
+  default     = {}
+}
+
+variable "private_subnet_tags" {
+  type        = map(string)
+  default     = {}
+}
+
+variable "public_route_table_tags" {
+  type        = map(string)
+  default     = {}
+}
+
+variable "private_route_table_tags" {
+  type        = map(string)
+  default     = {}
+}
